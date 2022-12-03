387178d5c65d0946e55adacf3b0242b48c5f8182
czubert <czubercik@gmail.com>
2022-12-03 11:37:13 +0000

    multiple languages were added

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,19 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 
 
 class Translator:
 
     def __init__(self):
         self.headers = {'User-Agent': 'Mozilla/5.0'}
-        self.direction = ''
+        self.__languages = {0: 'All', 1: 'Arabic', 2: 'German', 3: 'English', 4: 'Spanish', 5: 'French', 6: 'Hebrew',
+                            7: 'Japanese', 8: 'Dutch', 9: 'Polish', 10: 'Portuguese',
+                            11: 'Romanian', 12: 'Russian', 13: 'Turkish'}
+        self.source_lang = ''
+        self.direction_lang = ''
         self.word = ''
-        self.lang = ''
 
     def main(self):
         transl.greet()
-        transl.check_transl_direction()
         soup = transl.get_web_content()
         if soup:
             transl.get_translations(soup)
@@ -22,30 +23,28 @@ class Translator:
             print('Please try again')
 
     def greet(self):
-        welcome_msg = 'Type "en" if you want to translate from French into English, ' \
-                      'or "fr" if you want to translate from English into French:'
-
+        welcome_msg = 'Hello, welcome to the translator. Translator supports:'
         print(welcome_msg)
-        # self.direction = input()
-        self.direction = 'fr'
+        for i, el in self.__languages.items():
+            print(f"{i}. {el}")
+
+        print("Type the number of your language: ")
+        user_lang = int(input())
+        self.source_lang = self.__languages[user_lang]
+
+        print('Type the number of language you want to translate to:')
+        transl_lang = int(input())
+        self.direction_lang = self.__languages[transl_lang]
 
         print('Type the word you want to translate:')
-        # self.word = input()
-        self.word = 'hello'
+        self.word = input()
+        # self.word = 'dom'
 
-        print(f'You chose "{self.direction}" as the language to translate "{self.word}" to.')
-
-    def check_transl_direction(self):
-        if self.direction == 'en':
-            self.direction = 'french-english'
-            self.lang = 'English'
-        else:
-            self.direction = 'english-french'
-            self.lang = 'French'
+        print(f'You chose "{self.direction_lang}" as the language to translate "{self.word}" to.')
 
     def get_web_content(self):
-
-        url = f"https://context.reverso.net/translation/{self.direction}/{self.word}"
+        url_core = "https://context.reverso.net/translation"
+        url = f"{url_core}/{self.source_lang.lower()}-{self.direction_lang.lower()}/{self.word}"
 
         page = requests.get(url, headers=self.headers)
         conn_status = page.status_code
@@ -66,19 +65,27 @@ class Translator:
 
         for i in paragraphs:
             translations.append(i.text)
+
         print()
-        print(f'{self.lang} Translations:')
-        print(*translations, sep='\n', end='\n\n')
+        print(f'{self.direction_lang} Translations:')
+
+        if len(translations) == 0:
+            print("We are sorry, we do not support this pair of languagues")
+        else:
+            print(*translations, sep='\n', end='\n\n')
 
     def get_examples(self, soup):
         lines = soup.find_all('div', {'class': ['src ltr', 'trg ltr']})
-        print(f'{self.lang} Examples:')
+        print(f'{self.direction_lang} Examples:')
 
         for i, line in enumerate(lines):
             if i % 2 == 0 and i != 0:
                 print()
             tmp_example = (" ".join(line.text.split()))
-            print(tmp_example)
+            if len(tmp_example) == 0:
+                continue
+            else:
+                print(tmp_example)
 
 
 transl = Translator()
